53f852f0c2d2a50a82c4a25ca7c3657be9aa4408
Craig Christians <craig.christians@gmail.com>
2021-12-09 03:59:05 +0200

    v01.2 Base Pages Controller added

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -14,12 +14,51 @@ class Core
 
     public function __construct()
     {
-        $this->getUrl();
+        //print_r($this->getUrl());
+
+        //  Put the requested URL in the $url variable
+        $url = $this->getUrl();
+
+        //  Check controllers for a controller that matches what was requested in the url
+        //  If the param is posts, then check if a post controller exists
+        //  Look in controllers for first value
+        //  Check if the posts php file exists
+        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+
+            //  If it does exist, then set it as the current controller
+            //  That controller will then handle the request
+            $this->currentController = ucfirst($url[0]);
+
+            //  Then unset teh 0 index
+            unset($url[0]);
+        }
+
+        //  Require the controller that was set as the current controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        //  Then we need to instantiate that controller class
+        $this->currentController = new $this->currentController;
     }
 
     //  Gets the requested url
     public function getUrl()
     {
-        echo $_GET['url'];
+        //  If the url has a parameter
+        if(isset($_GET['url'])) {
+            //  Trim the trailing slash
+            $url = rtrim($_GET['url'], '/');
+
+            //  Sanitize as url
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            //  Break the URL params into an array using explode function
+            $url = explode('/', $url);
+
+            //  return the url once we've worked with it
+            return $url;
+        } else {
+            $pages = ['Pages'];
+            return $pages;
+        }
     }
 }
